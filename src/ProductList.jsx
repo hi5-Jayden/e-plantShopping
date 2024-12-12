2011acbe1dd3d9c5b76c664748abc8635ae2b542
hi5-Jayden <124787908+hi5-Jayden@users.noreply.github.com>
2024-12-12 05:37:31 -0500

    resolved logo linked

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -285,7 +285,7 @@ function ProductList() {
                 <div className="tag">
                    <div className="luxury">
                    <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                   <a href="/e-plantShopping/" onClick={handleLogoClick} style={{textDecoration:'none'}}>
+                   <a href="https://hi5-jayden.github.io/e-plantShopping/" onClick={handleLogoClick} style={{textDecoration:'none'}}>
                             <div>
                         <h3 style={{color:'white'}}>Paradise Nursery</h3>
                         <i style={{color:'white'}}>Where Green Meets Serenity</i>
